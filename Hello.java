0b604f94723bab1ba778b25cc5c080538951d5ef
NickP696 <pietrusn@go.stockton.edu>
2025-02-14 12:28:49 -0500

    lab3

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@
 public class Hello {
 	public static void main( String args[] )
 	 {
-	  System.out.println( "Hello, " + args[0] + "!" );
+		String wargs = "CSCI 4485";
+	  	System.out.println( "Hello, " + wargs + "!" );
 	 }
 
 }
